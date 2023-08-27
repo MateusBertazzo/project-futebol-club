8fb12874fd14adbbab9e0e9b7a46240bfe215c84
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-08-27 14:48:21 -0300

    implementa logica a rota getAll de Matches para obter quais partidas estão em progresso e quais já foram finalizadas de acordo com um query param

--- app/backend/src/controller/MatchesService.ts
+++ app/backend/src/controller/MatchesService.ts
@@ -3,6 +3,22 @@ import MatchesService from '../service/MatchesService';
 
 export default class MatchesController {
   static async getAllMatches(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query;
+
+    if (inProgress === 'true') {
+      const response = await MatchesService.getAllMatches();
+      const matchesInProgress = response
+        .data.filter((match) => match.inProgress === true);
+      return res.status(200).json(matchesInProgress);
+    }
+
+    if (inProgress === 'false') {
+      const response = await MatchesService.getAllMatches();
+      const matchesInFinish = response
+        .data.filter((match) => match.inProgress === false);
+      return res.status(200).json(matchesInFinish);
+    }
+
     const response = await MatchesService.getAllMatches();
 
     return res.status(200).json(response.data);
