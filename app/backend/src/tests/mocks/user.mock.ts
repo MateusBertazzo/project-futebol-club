e41769adadbd12bc07614bda5a1fb972f25c0eb2
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-08-27 13:07:33 -0300

    cobre testes em casos felizes da rota Login/User

---
+++ app/backend/src/tests/mocks/user.mock.ts
@@ -0,0 +1,24 @@
+const userMock = {
+  dataValues: {
+    id: 1,
+    username: 'Admin',
+    role: 'admin',
+    email: 'admin@admin.com',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+  },
+    id: 1,
+    username: 'Admin',
+    role: 'admin',
+    email: 'admin@admin.com',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+}
+
+const login = {
+  "email": "admin@admin.com",
+  "password": "secret_admin"
+};
+
+export {
+  userMock,
+  login,
+}
---
+++ app/backend/src/tests/users.test.ts
@@ -0,0 +1,42 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+import  User  from '../database/models/User';
+import { login, userMock } from './mocks/user.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testando Users', () => {
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('Testando Rota de Login caso passados Email and Password corretos', async () => {
+        sinon.stub(User, 'findOne').resolves(userMock as User);
+
+        const res: Response = await chai.request(app).post('/login').send(login);
+
+        expect(res.status).to.be.equal(200);
+        expect(res.body).to.have.property('token');
+    })
+
+    it('Testando Rota GET de login', async () => {
+        sinon.stub(User, 'findOne').resolves(userMock as User);
+
+        const { body: { token } }: Response = await chai.request(app).post('/login').send(login);
+
+        const res: Response = await chai.request(app).get('/login/role').set({ authorization: token }).send();
+
+        expect(res.status).to.be.equal(200);
+        expect(res.body).to.have.property('role');
+        expect(res.body.role).to.be.equal('admin');
+    });
+});
