e17ea059e9f6f19c19bc9efad24b30f7c07ad3b8
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-08-28 16:37:44 -0300

    teste de integração matches testando rota getAll

---
+++ app/backend/src/tests/matches.test.ts
@@ -0,0 +1,29 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+import { Response } from 'superagent';
+import Matche from '../database/models/Matche';
+import matchesMock from './mocks/matches.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testando Matches', () => {
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('Testando Rota getAll de matches', async () => {
+        sinon.stub(Matche, 'findAll').resolves(matchesMock as unknown as  Matche[]);
+
+        const res: Response = await chai.request(app).get('/matches');
+
+        expect(res.status).to.equal(200);
+        expect(res.body).to.deep.equal(matchesMock);
+    });
+})
---
+++ app/backend/src/tests/mocks/matches.mock.ts
@@ -0,0 +1,20 @@
+ const matchesMock = [
+    {
+        "id": 1,
+        "homeTeam": "Avaí/Kindermann",
+        "awayTeam": "Bahia",
+        "homeGoals": 1,
+        "awayGoals": 0,
+        "date": "2021-01-30T14:00:00.000Z"
+    },
+    {
+        "id": 2,
+        "homeTeam": "São Paulo",
+        "awayTeam": "Santos",
+        "homeGoals": 1,
+        "awayGoals": 1,
+        "date": "2021-01-30T14:00:00.000Z"
+    },
+]
+
+export default matchesMock;
